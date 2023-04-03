f4e16da93450a3fb36ea59df77da997f749bd09e
Your Name <you@example.com>
2023-04-03 11:04:21 +0200

    second comit

---
+++ PredavanjeDvaesetICetvrti/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+int a = 5;
+int b = 4;
+var c = a + b;
+var d = a - b;
+var e = a * b;
+var r = a / b;
+
+Console.WriteLine("Rezultatot e: {0} {1} {2} {3}", c, d, e, r);
+
+// Vtora zadaca
+string phrase = "Pisuvam programa";
+string phrase1 = "vo c#";
+
+string phrase2 = phrase + " " + phrase1;
+Console.WriteLine(phrase2);
+
+
+// Treta zadaca
+
+/*
+int a = 5;
+int b = 6;
+if(a == b)
+{
+    Console.WriteLine("Equal");
+}
+else
+{
+    Console.WriteLine("Not Equal:");
+
+}
+*/
+
